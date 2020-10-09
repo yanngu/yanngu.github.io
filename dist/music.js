6024bba468e4009c27d2422d713ce45e1e686961
Yann Gu <guyq2014@foxmail.com>
2020-10-09 18:49:06 +0800

    Site updated: 2020-10-09 18:49:05

---
+++ dist/music.js
@@ -0,0 +1,35 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    loop: 'all',
+    volume: 0.5,
+    listFolded: true,
+    listMaxHeight: 60,
+    audio: [
+        {
+            name: '旅行',
+            artist: '许巍',
+            url: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/audio/lvxing.mp3',
+            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/audio_img.png',
+        },
+        {
+            name: '曾经的你',
+            artist: '许巍',
+            url: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/audio/cengjingdeni.mp3',
+            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/audio_img.png',
+        },
+        {
+            name: '蓝莲花',
+            artist: '许巍',
+            url: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/audio/lanlianhua.mp3',
+            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/audio_img.png',
+        },
+        {
+            name: '年少有为',
+            artist: '李荣浩',
+            url: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/audio/nianshaoyouwei.mp3',
+            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/audio_img.png',
+        }
+    ]
+});
