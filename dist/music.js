2a34f087efb3057d2b52bfa51deb12d418ccc020
Yann Gu <guyq2014@foxmail.com>
2020-10-14 20:37:54 +0800

    Site updated: 2020-10-14 20:37:53

--- dist/music.js
+++ dist/music.js
@@ -11,25 +11,25 @@ const ap = new APlayer({
             name: '旅行',
             artist: '许巍',
             url: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/audio/lvxing.mp3',
-            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/audio_img.png',
+            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/default.png',
         },
         {
             name: '曾经的你',
             artist: '许巍',
             url: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/audio/cengjingdeni.mp3',
-            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/audio_img.png',
+            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/default.png',
         },
         {
             name: '蓝莲花',
             artist: '许巍',
             url: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/audio/lanlianhua.mp3',
-            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/audio_img.png',
+            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/default.png',
         },
         {
             name: '年少有为',
             artist: '李荣浩',
             url: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/audio/nianshaoyouwei.mp3',
-            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/audio_img.png',
+            cover: 'https://cdn.jsdelivr.net/gh/yanngu/cdn/img/bg/default.png',
         }
     ]
 });
